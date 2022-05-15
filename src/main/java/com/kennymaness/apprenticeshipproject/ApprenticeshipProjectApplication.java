27ffa2de05f85cee47e08dd09b165bbe82a36490
Kenny Maness <kennymaness@Kennys-Mac-mini-1473.lan>
2022-05-14 19:44:41 -0500

    first commit

---
+++ src/main/java/com/kennymaness/apprenticeshipproject/ApprenticeshipProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kennymaness.apprenticeshipproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprenticeshipProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApprenticeshipProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kennymaness/apprenticeshipproject/ApprenticeshipProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kennymaness.apprenticeshipproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprenticeshipProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
